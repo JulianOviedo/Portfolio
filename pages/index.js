bc52e30d062b51e61d392a8a9a7a434958fa8cff
Julian <julianovie234@gmail.com>
2022-12-15 12:51:41 -0300

    add navitem component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Background from '../src/components/Background/Background'
 import styles from '../styles/Home.module.css'
+import Navbar from '../src/components/NavBar/NavBar'
 
 export default function Home () {
   return (
     <>
       <Background>
+        <Navbar/>
         <div className={styles.titles}>
           <h1> My Porfolio</h1>
         </div>
