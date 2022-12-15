b5140c52e963d3f9965c87a4067c9a8d06225690
Julian <julianovie234@gmail.com>
2022-12-15 10:38:30 -0300

    delete unnecesary code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { Roboto_Mono } from "@next/font/google";
+import "../styles/globals.css";
+
+const font = Roboto_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
