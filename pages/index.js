903eeb3726b938c513d6fab455bb8d267eac46cc
JulianOvie <julianovie234@gmail.com>
2022-12-24 16:52:17 -0300

    add avatar mui

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
+import { Avatar } from '@mui/material'
 import Balloon from '../src/components/Balloon/Balloon'
 import styles from '../styles/Home.module.css'
 
 export default function Home () {
   return (
     <>
+      <div className={styles['avatar-container']}>
+        <Avatar sx={{ width: 150, height: 150 }} alt='profile pic' src='/profile-img.jpg'/>
+      </div>
       <h1 className={styles.h1}>Julian Oviedo Portfolio</h1>
       <div className={styles['cvs-container']}>
-        <a className={styles['cvs-download']} download="downloadResumeSpanish" href="../public/Cv Julian Oviedo Spanish.pdf">Cv Spanish</a>
-        <a className={styles['cvs-download']} download="downloadResumeEnglish" href="../public/Cv Julian Oviedo English.pdf">Cv English</a>
+        <a className={styles['cvs-download']} download="downloadResumeSpanish" href="/public/Cv Julian Oviedo Spanish.pdf">Cv Spanish</a>
+        <a className={styles['cvs-download']} download="downloadResumeEnglish" href="/public/Cv Julian Oviedo English.pdf">Cv English</a>
       </div>
       <footer className={styles.footer}>
         <Balloon/>
