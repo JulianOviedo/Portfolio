128139655fc01cb0a9a4174edaf62dd58a2bb6b7
JulianOvie <julianovie234@gmail.com>
2022-12-24 16:21:44 -0300

    add balloon contact me icon

--- src/components/Balloon/Balloon.js
+++ src/components/Balloon/Balloon.js
@@ -1,9 +1,13 @@
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
+import ContactMailIcon from '@mui/icons-material/ContactMail'
+import Link from 'next/link'
 
 export default function Balloon () {
   return (
-    <ChatBubbleOutlineOutlinedIcon>
-      <h1>Contact me !</h1>
-    </ChatBubbleOutlineOutlinedIcon>
+    <Link href="/contact">
+      <ContactMailIcon sx={{
+        width: 55, height: 55, m: '20px', color: '#ffffff'
+      }}>
+      </ContactMailIcon>
+    </Link>
   )
 }
