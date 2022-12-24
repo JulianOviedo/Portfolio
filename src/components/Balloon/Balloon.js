45242afc2d3d145af4132037a0f25b8e51f2ba25
JulianOvie <julianovie234@gmail.com>
2022-12-24 16:00:12 -0300

    add ballon component

---
+++ src/components/Balloon/Balloon.js
@@ -0,0 +1,9 @@
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
+
+export default function Balloon () {
+  return (
+    <ChatBubbleOutlineOutlinedIcon>
+      <h1>Contact me !</h1>
+    </ChatBubbleOutlineOutlinedIcon>
+  )
+}
