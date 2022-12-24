586873a13feedc64e8b3b1d9bf80220877be2d8d
JulianOvie <julianovie234@gmail.com>
2022-12-24 15:40:24 -0300

    delete navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Background from '../src/components/Background/Background'
 import styles from '../styles/Home.module.css'
-import Navbar from '../src/components/NavBar/NavBar'
 
 export default function Home () {
   return (
     <>
       <Background>
-        <Navbar/>
         <div className={styles.titles}>
           <h1> My Portfolio</h1>
         </div>
