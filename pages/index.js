d859a53fa1fdd6ed818cfe802fae76999d21e7fb
JulianOvie <julianovie234@gmail.com>
2022-12-24 18:29:09 -0300

    refact text style in about me card

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import Card from '../src/components/Card/Card'
 import styles from '../styles/Home.module.css'
 
 export default function Home () {
+  const descriptionAboutMe = ' I am finishing the third year of my bachelor\'s degree in computer science and in search of a job position where I can continue to grow, learn and evolve both professionally and personally, exploiting my skills and abilities through commitment and love for what I do and study. \n\n I have solid knowledge in some frameworks like React.JS, Next.JS, Redux, Express and also, know Node.JS \n\n I consider myself a very capable person to achieve everything i propose to and always want to learn new things. I think i only need the oportunity to introduce myself and show what I\'m worth\''
   return (
     <>
-      <div className={styles['avatar-container']}>
+      <header className={styles['avatar-container']}>
         <Avatar sx={{ width: 150, height: 150 }} alt='profile pic' src='/profile-img.jpg'/>
-      </div>
+      </header>
       <h1 className={styles.h1}>Julian Oviedo Portfolio</h1>
       <div className={styles['cvs-container']}>
         <a className={styles['cvs-download']} download="downloadResumeSpanish" href="/public/Cv Julian Oviedo Spanish.pdf">Cv Spanish</a>
@@ -17,9 +18,7 @@ export default function Home () {
       <div className={styles['card-container']}>
         <Card
           title="About me"
-          description='I am finishing the third year of my bachelor/s degree in computer science and in search of a job position where I can continue to grow, learn and evolve both professionally and personally, exploiting my skills and abilities through commitment and love for what I do and study.
-          I have solid knowledge in some frameworks like React.JS, Next.JS, Redux, Express and also, know Node.JS\n
-          I consider myself a very capable person to achieve everything i propose to and always want to learn new things. I think i only need the oportunity to introduce myself and show what I/m worth/'/>
+          description={descriptionAboutMe}/>
       </div>
       <footer className={styles.footer}>
         <Balloon/>
