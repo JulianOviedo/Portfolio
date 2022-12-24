e48ba404eec3d978a356fd92bc9e4adae382ec9d
JulianOvie <julianovie234@gmail.com>
2022-12-24 17:06:05 -0300

    card component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Avatar } from '@mui/material'
 import Balloon from '../src/components/Balloon/Balloon'
+import Card from '../src/components/Card/Card'
 import styles from '../styles/Home.module.css'
 
 export default function Home () {
@@ -12,6 +13,9 @@ export default function Home () {
       <div className={styles['cvs-container']}>
         <a className={styles['cvs-download']} download="downloadResumeSpanish" href="/public/Cv Julian Oviedo Spanish.pdf">Cv Spanish</a>
         <a className={styles['cvs-download']} download="downloadResumeEnglish" href="/public/Cv Julian Oviedo English.pdf">Cv English</a>
+      </div>
+      <div className={styles['card-container']}>
+        <Card/>
       </div>
       <footer className={styles.footer}>
         <Balloon/>
---
+++ src/components/Card/Card.js
@@ -0,0 +1,7 @@
+import styles from './Card.module.css'
+
+export default function Card () {
+  return (
+    <div className={styles.card}></div>
+  )
+}
