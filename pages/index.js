e170a9efcd45c3009d265749c9c0dba3ff84ac16
JulianOvie <julianovie234@gmail.com>
2022-12-24 16:40:55 -0300

    downloads <a/> and styles

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ export default function Home () {
   return (
     <>
       <h1 className={styles.h1}>Julian Oviedo Portfolio</h1>
+      <div className={styles['cvs-container']}>
+        <a className={styles['cvs-download']} download="downloadResumeSpanish" href="../public/Cv Julian Oviedo Spanish.pdf">Cv Spanish</a>
+        <a className={styles['cvs-download']} download="downloadResumeEnglish" href="../public/Cv Julian Oviedo English.pdf">Cv English</a>
+      </div>
       <footer className={styles.footer}>
         <Balloon/>
       </footer>
