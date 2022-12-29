355d5ad4369c06437c1db731ec7f2784f61b3c57
JulianOvie <julianovie234@gmail.com>
2022-12-29 00:18:22 -0300

    change the approach of card

--- src/components/Slides/Slides.js
+++ src/components/Slides/Slides.js
@@ -1,12 +1,6 @@
 import Carousel from '../Carousel/Carousel'
 import Card from '../Card/Card'
-import styles from '../../../styles/Home.module.css'
-
-const descriptionAboutMe = ' 👋  Hi!  My name is Julian Oviedo, I\'m 26 years old \n\n 🌍  I\'m based in Córdoba, Argentina \n\n I\'m finishing the third year of my bachelor\'s degree in computer science and in search of a job position where I can continue to grow, learn and evolve both professionally and personally, exploiting my skills and abilities through commitment and love for what I do and study. \n\n I have solid knowledge in some frameworks like React.JS, Next.JS, Redux, Express and also, know Node.JS \n\n I consider myself a very capable person to achieve everything i propose to and always want to learn new things. I think i only need the oportunity to introduce myself and show what I\'m worth '
-const linkExperience = 'https://github.com/qaprosoft/react-laba-arg-1'
-const descriptionExperience = ' 💡 Software Developer at Solvd Inc. \n\n FROM > January 2022 \n TO > Present \n\n 💡 React Javascript Intern at Solvd Inc. \n\n FROM > June 2022 \n TO > December 2022 \n\n - Content and progress at '
-const descriptionProyects1 = ' 📚 Shoes Shop - Ecommerce mock \n\n -A responsive app created by Next.js using mainly React.js and MUI, also other libraries like Axios & ReactQuery to connect the API to the app.   \n\n 📂 To see the code '
-const linkProyect1 = 'https://github.com/JulianOviedo/Shoes-Shop'
+import styles from '../Slides/Slides.module.css'
 
 export default function Slides () {
   return (
@@ -14,21 +8,33 @@ export default function Slides () {
       <div className={styles['card-container']}>
         <Card
           title="About me"
-          description={descriptionAboutMe}/>
+        >
+          <p className={styles['card-description']}>👋  Hi!  My name is Julian Oviedo, I'm 26 years old <br/><br/> 🌍  I'm based in Córdoba, Argentina <br/><br/> I'm finishing the third year of my bachelor's degree in computer science and in search of a job position where I can continue to grow, learn and evolve both professionally and personally, exploiting my skills and abilities through commitment and love for what I do and study. <br/><br/> I have solid knowledge in some frameworks like React.JS, Next.JS, Redux, Express and also, know Node.JS <br/><br/> I consider myself a very capable person to achieve everything i propose to and always want to learn new things. I think i only need the oportunity to introduce myself and show what I'm worth</p>
+        </Card>
       </div>
       <div className={styles['card-container']}>
         <Card
           title="Experience"
-          description={descriptionExperience}
-          link={linkExperience}/>
+        >
+          <p className={styles['card-description']}> 💡 Software Developer at Solvd Inc. <br/><br/> FROM {'>'} January 2022 <br/> TO {'>'} Present <br/><br/> 💡 React Javascript Intern at Solvd Inc. <br/><br/> FROM {'>'} June 2022 <br/> TO {'>'} December 2022 <br/><br/> - Content and progress at <a href='https://github.com/qaprosoft/react-laba-arg-1'>Click here!</a></p>
+        </Card>
       </div>
       <div className={styles['card-container']}>
         <Card
           title="Personal Proyects"
-          gif={'/shoesShop-video.mp4'}
-          description={descriptionProyects1}
-          link={linkProyect1}>
+        >
+          <div className={styles.video}> <video muted autoPlay loop width={600} height={350}>
+            <source src={'/shoesShop-video.mp4'} type="video/mp4" />
+          </video></div>
+          <p className={styles['card-description']}> 📚 Shoes Shop - Ecommerce mock <br/><br/> -A responsive app reated by Next.js using mainly React.js and MUI, also other libraries like Axios & ReactQuery to connect the API to the app.   <br/><br/> 📂 To see the code <a href='https://github.com/JulianOviedo/Shoes-Shop'>Click here!</a></p>
           <p className={styles.p}> 🖥️ To see deployment <a href='www.shoes-shop-kyl2.vercel.app'>Go here!</a></p>
+          <hr/>
+          <br/>
+          <div className={styles.video}> <video muted autoPlay loop width={600} height={350}>
+            <source src={'/shoesShop-video.mp4'} type="video/mp4" />
+          </video></div>
+          <p className={styles['card-description']}> 📚 Calculator App <br/><br/> Just a Calculator with the android mobile design with MUI and React.js<br/><br/> 📂 To see the code <a href='https://github.com/JulianOviedo/CalculatorApp'>Click here!</a></p>
+          <p className={styles.p}> 🖥️ To see deployment <a href='https://calculator-app-df6a9.web.app/'>Go here!</a></p>
         </Card>
       </div>
     </Carousel>
