a43fb831f0461343ea3777f8c01998de6f17161c
JulianOviedo <you@example.com>
2023-01-04 02:33:05 -0300

    replace icon contact for button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,9 +5,9 @@ export default function Balloon({children}) {
   return (
     <Link href="/contact" style={{ textDecoration: 'none' }}>
       <Button variant="contained" size='large' sx={{
-         m: '20px', backgroundColor: 'gray', color: 'black', textDecoration:'none', '&:hover': {
-      backgroundColor: '#ffffff',
-      color: 'black'}
+         m: '20px', backgroundColor: 'white', color: 'black', textDecoration:'none', '&:hover': {
+      backgroundColor: 'gray',
+      color: 'white'}
       }}>
         {children}
       </Button>
