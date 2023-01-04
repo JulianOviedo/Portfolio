bc0e0249501666a111304d4f76ed666f583a49f2
JulianOviedo <you@example.com>
2023-01-04 02:34:31 -0300

    fix socialmedia component

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,9 +1,8 @@
 import Link from 'next/link';
-import styles from './SocialMedia.module.css'
 
 export default function SocialMedia ({link, children}) {
     return (
-        <div className={styles['social-media-icon']}>
+        <div >
             <Link href={link}>
                 {children}
             </Link>
