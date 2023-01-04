e4a1376310d5274fe98ffa9c3d2482e5565ab202
JulianOviedo <julianovie234@gmail.com>
2023-01-04 12:36:55 -0300

    asd

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -14,7 +14,7 @@ function Carousel({ children }) {
         itemsToShow={1}
         itemsToScroll={1}
         forwardBtnProps={{
-          // here you can also pass className, or any other button element attributes
+          // here you can also pass className, or any other button element attribute
           style: {
             alignSelf: 'center',
             background: 'gray',
