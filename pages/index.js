455d19d5f2adde5ff0174034b49e8f58d7a1ad3c
JulianOviedo <julianovie234@gmail.com>
2023-01-04 13:25:14 -0300

    add github socialmedia

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import SocialMedia from '../src/components/SocialMedia/SocialMedia'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Home() {
   return (
@@ -29,6 +30,9 @@ export default function Home() {
           </SocialMedia>
           <SocialMedia link='https://www.instagram.com/julian.oviedo.7/' >
             <InstagramIcon sx={{ fontSize: "60px" }}/>
+          </SocialMedia>
+          <SocialMedia link='https://github.com/JulianOviedo' >
+            <GitHubIcon sx={{ fontSize: "60px" }}/>
           </SocialMedia>
         </div>
         <Button > Contact Me </Button>
