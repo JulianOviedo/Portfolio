6b8b4494124ebec30e64c5f4869173281b0ce6e5
JulianOviedo <you@example.com>
2023-01-04 02:38:09 -0300

    delete unnecesary props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import styles from './Card.module.css'
 
-export default function Card({ title, description, link, children, gif }) {
+export default function Card({ title, children }) {
   return (
     <div className={styles.card}>
       <h2 className={styles['card-title']}>{title}</h2>
