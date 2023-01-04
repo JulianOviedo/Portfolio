d97bf9e108db780eb77d93a708bc780691bc12e0
JulianOviedo <julianovie234@gmail.com>
2023-01-04 12:46:42 -0300

    resize contact page content

--- pages/contact.js
+++ pages/contact.js
@@ -33,30 +33,33 @@ export default function Contact() {
     e.target.reset()
   };
   return (
-    <>
-      <h1 className={styles.h1}>You can contact me through this form:</h1>
-      <form ref={form} onSubmit={sendEmail} className={styles.form}>
-        <label>
-          <p className={styles['form-label']}>Name *</p>
-          <input type="text" name="user_name" required className={styles.input} />
-        </label>
-        <label>
-          <p className={styles['form-label']}>Email *</p>
-          <input type="email" name="user_email" required className={styles.input} />
-        </label>
-        <label>
-          <p className={styles['form-label']}>Message *</p>
-          <textarea name="message" required className={styles.textarea} />
-        </label>
-        <input type='submit' className={styles['form-button']} value='Send Email' />
-      </form>
-      <footer className={styles.footer}>
-        <Link href="/">
-          <ArrowBackIosNewIcon fontSize='large' sx={{
-            width: 55, height: 55, m: '20px', color: '#ffffff', cursor: 'pointer'
-          }}></ArrowBackIosNewIcon>
-        </Link>
-      </footer>
-    </>
+    <div className={styles['contact-container']}>
+      <div className={styles['contact-container__info']}></div>
+      <div className={styles['contact-container__form']}>
+        <h1 className={styles.h1}>You can contact me through this form:</h1>
+        <form ref={form} onSubmit={sendEmail} className={styles.form}>
+          <label>
+            <p className={styles['form-label']}>Name *</p>
+            <input type="text" name="user_name" required className={styles.input} />
+          </label>
+          <label>
+            <p className={styles['form-label']}>Email *</p>
+            <input type="email" name="user_email" required className={styles.input} />
+          </label>
+          <label>
+            <p className={styles['form-label']}>Message *</p>
+            <textarea name="message" required className={styles.textarea} />
+          </label>
+          <input type='submit' className={styles['form-button']} value='Send Email' />
+        </form>
+        <footer className={styles.footer}>
+          <Link href="/">
+            <ArrowBackIosNewIcon fontSize='large' sx={{
+              width: 55, height: 55, m: '20px', color: '#ffffff', cursor: 'pointer'
+            }}></ArrowBackIosNewIcon>
+          </Link>
+        </footer>
+      </div>
+    </div>
   )
 }
