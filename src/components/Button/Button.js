6b54ad6dff3455e7d2c63545a4e3a4b8f2454ecf
JulianOvie <julianovie234@gmail.com>
2023-01-06 11:38:50 -0300

    last styles for responsive home page

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,11 @@
 import { Button } from '@mui/material'
 import Link from 'next/link'
+import styles from './Button.module.css'
 
 export default function Balloon({children}) {
   return (
     <Link href="/contact" style={{ textDecoration: 'none' }}>
-      <Button variant="contained" size='large' sx={{
+      <Button className={styles['contact-button']} variant="contained" size='large' sx={{
          fontFamily:'inherit !important', m: '20px', backgroundColor: 'white', color: 'black', textDecoration:'none', '&:hover': {
       backgroundColor: 'gray',
       color: 'white'}
