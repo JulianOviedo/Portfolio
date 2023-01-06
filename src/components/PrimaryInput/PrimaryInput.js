d7861aa9e3c4163a3f5dc66cec3ab2941c9ea8f3
JulianOvie <julianovie234@gmail.com>
2023-01-06 13:18:51 -0300

    create input reusable component

---
+++ src/components/PrimaryInput/PrimaryInput.js
@@ -0,0 +1,17 @@
+import styles from './PrimaryInput.module.css'
+
+export default function PrimaryInput({ label, placeholder, type, name }) {
+    return (
+        <label className={styles.label}>
+            <p>{label}</p>
+            <input 
+                type={type} 
+                className={type === 'textarea' ?
+                styles.textarea : type === 'submit' ?
+                styles['submit-button'] : styles.input}
+                name={name}
+                placeholder={placeholder}
+                required></input>
+        </label>
+    )
+}
