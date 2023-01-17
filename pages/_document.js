12fad880f9c43b25c6a83c6cb0fd38a083c5931e
JulianOviedo <julianovie234@gmail.com>
2023-01-17 11:12:16 -0300

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-92M5BH56XV"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-92M5BH56XV');
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
