f330601b902472ce3a14fdcd9ecf6cdddc2a2fdb
JulianOviedo <julianovie234@gmail.com>
2023-02-04 11:05:26 -0300

    delete broken code from google analitycs

--- pages/_document.js
+++ pages/_document.js
@@ -3,16 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-92M5BH56XV"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-92M5BH56XV');
-        </script>
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
