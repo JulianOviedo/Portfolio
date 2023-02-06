f6539ac82e61897ae3b6c4910f8255008747f580
JulianOviedo <julianovie234@gmail.com>
2023-02-06 17:00:03 -0300

    ref mui

--- mui-theme.js
+++ mui-theme.js
@@ -4,9 +4,30 @@ export const theme = createTheme({
   typography: {
     download: {
       margin: '10px',
+      width: '100%',
       border: '2px solid #ffffff',
       borderRadius: '10px',
-      padding: '15px'
+      padding: '15px',
+      textDecoration: 'none',
+      display: 'flex',
+      justifyContent: 'center',
+      '&:hover': {
+        animation: 'pulse 1.5s',
+        cursor: 'pointer'
+      },
+      '@keyframes pulse': {
+        '0%': {
+          transform: 'scale(.85)'
+        },
+        '70%': {
+          transform: 'scale(1)',
+          boxShadow: '0 0 0 50px rgba(#5a99d4, 0)'
+        },
+        '100%': {
+          transform: 'scale(1)',
+          boxShadow: '0 0 0 0 rgba(#5a99d4, 0)'
+        }
+      }
     }
   },
   breakpoints: {
@@ -20,11 +41,10 @@ export const theme = createTheme({
 })
 
 theme.typography.h1 = {
-  fontWeight: 600,
-  fontSize: '40px',
-  color: 'red',
+  fontWeight: 'bold',
+  fontSize: '2em',
   [theme.breakpoints.down('md')]: {
-    fontSize: '35px',
-    fontWeight: 700
+    // fontSize: '35px',
+    // fontWeight: 700
   }
 }
