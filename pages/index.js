9f393437be352fcf967207c91309039962e8c298
JulianOviedo <julianovie234@gmail.com>
2023-02-07 16:55:02 -0300

    render correctly

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
 import Slides from '../src/components/Slides/Slides'
 import Header from '../src/components/Header/Header'
 import Footer from '../src/components/Footer/Footer'
-import { Container } from '@mui/material'
+import { Box, useTheme } from '@mui/material'
 import Contact from '../src/components/Contact/Contact'
 import Stack from '../src/components/Stack/Stack'
 
 export default function Home() {
+  const theme = useTheme()
   return (
     <>
       <Header/>
-      <Container sx={{ }}>
-        <Slides />
-        <Container sx={{ display: 'grid', gridTemplateColumns: '50% 50%', mt: '100px' }}>
-          <Contact/>
-          <Stack/>
-        </Container>
-      </Container>
-      {/* <Footer/> */}
+      <Slides />
+      <Box sx={{
+        display: 'grid',
+        gridTemplateColumns: '50% 50%',
+        mt: '100px',
+        py: '24px',
+        [theme.breakpoints.down('md')]: {
+          display: 'flex',
+          flexDirection: 'column'
+        }
+      }}>
+        <Contact/>
+        <Stack/>
+      </Box>
+      <Footer/>
     </>
   )
 }
