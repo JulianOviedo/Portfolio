a53a6b3950095c34a553c1a22954e5f3f2e426b2
JulianOviedo <julianovie234@gmail.com>
2023-02-07 12:45:38 -0300

    ref sizes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <Avatar sx={{ width: 150, height: 150 }} alt='profile pic' src='/profile-img.jpg' />
         <Box component='section' sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
           <Typography variant='h1'>Julian Oviedo Portfolio</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
             <Typography variant='download' component='a' download="downloadResumeSpanish" href="/Cv Julian Oviedo Spanish.pdf">Cv Spanish <FileDownloadIcon sx={{ ml: '5px' }} /></Typography>
             <Typography variant='download' component='a' download="downloadResumeEnglish" href="/Cv Julian Oviedo English.pdf">Cv English <FileDownloadIcon sx={{ ml: '5px' }} /></Typography>
           </Box>
