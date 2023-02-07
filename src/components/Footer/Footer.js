55e9c61a48f09e83aa476f02102cdeb76d3bd992
JulianOviedo <julianovie234@gmail.com>
2023-02-07 16:54:15 -0300

    ref footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,25 +3,31 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import MailOutlineIcon from '@mui/icons-material/MailOutline'
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone'
 
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Footer = () => {
   return (
-    <Box component='footer' sx={{ height: 100, p: '20px', position: 'fixed', bottom: '0', width: '100%', display: 'flex' }}>
-      <Box component='nav' sx={{ display: 'flex', flexDirection: 'row', width: '100%', gap: '10px' }}>
+    <Box component='footer' sx={{ height: 100, p: '20px', width: '100%', display: 'flex', mt: '40px' }}>
+      <Box component='nav' sx={{ display: 'flex', flexDirection: 'row', width: '100%', gap: '10px', alignItems: 'flex-end' }}>
         <SocialMedia link='https://www.linkedin.com/in/julian-oviedo-17b605136/' >
-          <LinkedInIcon sx={{ fontSize: '25px' }} />
+          <LinkedInIcon sx={{ fontSize: '30px' }} />
         </SocialMedia>
         <SocialMedia link='https://www.facebook.com/julian.oviedo.7' >
-          <FacebookIcon sx={{ fontSize: '25px' }} />
+          <FacebookIcon sx={{ fontSize: '30px' }} />
         </SocialMedia>
         <SocialMedia link='https://www.instagram.com/julian.oviedo.7/' >
-          <InstagramIcon sx={{ fontSize: '25px' }} />
+          <InstagramIcon sx={{ fontSize: '30px' }} />
         </SocialMedia>
         <SocialMedia link='https://github.com/JulianOviedo' >
-          <GitHubIcon sx={{ fontSize: '25px' }} />
+          <GitHubIcon sx={{ fontSize: '30px' }} />
         </SocialMedia>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '5px', justifyContent: 'flex-end' }}>
+        <Typography variant='contact' sx={{ display: 'flex', gap: '5px', alignItems: 'center' }}><MailOutlineIcon fontSize='small'/>julianovie234@gmail.com</Typography>
+        <Typography variant='contact' sx={{ display: 'flex', gap: '5px', alignItems: 'center' }}><PhoneIphoneIcon fontSize='small'/>+54 9 351 761-9357</Typography>
       </Box>
     </Box>
   )
