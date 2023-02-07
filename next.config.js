2e4718c2c42d3426ce66416982d6ff0eb27fafb3
JulianOviedo <julianovie234@gmail.com>
2023-02-07 10:24:54 -0300

    Refact next.config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'raw.githubusercontent.com'
+      // add any other domains you need here
+    ]
+  }
 }
 
 module.exports = nextConfig
