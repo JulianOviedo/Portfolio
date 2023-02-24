ad0a9bad68304dd52d7a10c23d4933ef66dcd314
JulianOviedo <julianovie234@gmail.com>
2023-02-24 14:45:53 -0300

    ref url

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -7,6 +7,7 @@ const Stack = () => {
   const baseURL = 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills'
 
   const theme = useTheme()
+  // box-shadow: 0 0 0 5px rgba(255, 255, 255, 0.6), 0 0 0 10px rgba(255, 255, 255, 0.3);
 
   return (
     <>
@@ -17,43 +18,43 @@ const Stack = () => {
         }
       }}>
         <Typography variant='h1' sx={{ textAlign: 'center', mt: '50px' }}>Stack</Typography>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '18%', right: '5%', '&:hover::after': { content: '"Typescript"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '18%', right: '5%', '&:hover::after': { content: '"Typescript"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Typescript' title='Typescript' src={`${baseURL}/typescript-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '30%', right: '20%', '&:hover::after': { content: '"Javascript"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '30%', right: '20%', '&:hover::after': { content: '"Javascript"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Javascript' title='Javascript' src={`${baseURL}/javascript-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '65%', right: '33%', '&:hover::after': { content: '"HTML"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '65%', right: '33%', '&:hover::after': { content: '"HTML"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='html' title='html' src={`${baseURL}/html5-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '45%', left: '23%', '&:hover::after': { content: '"React.js"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '45%', left: '23%', '&:hover::after': { content: '"React.js"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='React.js' title='React.js' src={`${baseURL}/react-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '25%', left: '9%', '&:hover::after': { content: '"Next.js"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '25%', left: '9%', '&:hover::after': { content: '"Next.js"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Next.js' title='Next.js' src={`${baseURL}/nextjs-colored-dark.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '54%', left: '42%', '&:hover::after': { content: '"CSS"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '54%', left: '42%', '&:hover::after': { content: '"CSS"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='CSS' title='CSS' src={`${baseURL}/css3-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '80%', left: '15%', '&:hover::after': { content: '"Bootstrap"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '80%', left: '15%', '&:hover::after': { content: '"Bootstrap"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Bootstrap' title='Bootstrap' src={`${baseURL}/bootstrap-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '62%', left: '9%', '&:hover::after': { content: '"MaterialUI"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '62%', left: '9%', '&:hover::after': { content: '"MaterialUI"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Material UI' title='Material UI' src={`${baseURL}/materialui-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '10%', left: '43%', '&:hover::after': { content: '"Redux"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '10%', left: '43%', '&:hover::after': { content: '"Redux"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Redux' title='Redux' src={`${baseURL}/redux-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '53%', right: '12%', backgroundColor: 'white', borderRadius: '50%', p: '5px', py: '5px', '&:hover::after': { content: '"Express"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '53%', right: '12%', backgroundColor: 'white', borderRadius: '50%', p: '5px', py: '5px', '&:hover::after': { content: '"Express"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Express' title='Express' src={`${baseURL}/express-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '78%', right: '12%', '&:hover::after': { content: '"Node.js"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '78%', right: '12%', '&:hover::after': { content: '"Node.js"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Node.js' title='Node.js' src={`${baseURL}/nodejs-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '75%', right: '45%', '&:hover::after': { content: '"Taildwind"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '75%', right: '45%', '&:hover::after': { content: '"Taildwind"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Node.js' title='Node.js' src={`${baseURL}/tailwindcss-colored.svg`} width={55} height={55}></Image>
         </Box>
-        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '30%', right: '55%', '&:hover::after': { content: '"MongoDB"' } }}>
+        <Box className={styles.skill} sx={{ position: 'absolute', bottom: '30%', right: '55%', '&:hover::after': { content: '"MongoDB"' }, '&:hover': { filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.8))', scale: '1.1' } }}>
           <Image alt='Node.js' title='Node.js' src={`${baseURL}/mongodb-colored.svg`} width={55} height={55}></Image>
         </Box>
       </Container>
